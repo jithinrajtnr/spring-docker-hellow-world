d3fc4f97a0e696e5ac6666d886c7edcfcbb7378b
jithinraj-mac <jithinrajktd@gmail.com>
2020-07-07 11:58:43 +0530

    zzx

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.gazgeek.helloworld.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${greeting.message}")
+    private String message;
+
     @RequestMapping("/")
     String home() {
-        return "Hello from GazGeek!";
+        return "Message is : " + message;
     }
 
 }
