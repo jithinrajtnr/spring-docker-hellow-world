61ba7aa8a398eb4599cb1c36253a5bd1b1558022
jithinraj-mac <jithinrajktd@gmail.com>
2020-07-08 16:00:10 +0530

    qaz

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.gazgeek.helloworld.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @Value("${greeting.message}")
     private String message;
 
     @RequestMapping("/")
     String home() {
+
+        logger.info("Request arrived in home mappings");
         return "Message is : " + message;
     }
 
